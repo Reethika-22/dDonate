1a2cc7e5081302ee1fdf4f37b7545b4b5224aa13
reethika <reethika.22.arasavilli@gmail.com>
2025-11-02 12:34:51 +0530

    Updated feature or fixed issue

--- projects/algocharity-contracts/smart_contracts/algo_charity/contract.py
+++ projects/algocharity-contracts/smart_contracts/algo_charity/contract.py
@@ -1,5 +1,6 @@
 from algopy import ARC4Contract, UInt64, arc4, GlobalState, LocalState, Txn, Account
 
+
 class DonationContract(ARC4Contract):
     """
     dDonate Smart Contract
@@ -10,17 +11,17 @@ class DonationContract(ARC4Contract):
 
     def __init__(self) -> None:
         # Total Algos donated across all causes
-        self.total_donations = GlobalState(UInt64(0), key="total_donations", description="Total Algos donated in all causes")
+        self.total_donations = GlobalState(UInt64, key="total_donations", description="Total Algos donated in all causes")
 
         # Total number of registered causes
-        self.cause_count = GlobalState(UInt64(0), key="cause_count", description="Number of causes registered")
+        self.cause_count = GlobalState(UInt64, key="cause_count", description="Number of causes registered")
 
         # Donation amount by each donor (local state)
         self.donor_donations = LocalState(UInt64, key="donor_donations", description="Total donations made by each donor")
 
-        # Cause-specific balances and owners (using separate global keys)
-        self.cause_balance = GlobalState(UInt64(0), key="cause_balance", description="Balance for the current cause")
-        self.cause_owner = GlobalState(Account(), key="cause_owner", description="Owner of the cause")
+        # Cause-specific balances and owners
+        self.cause_balance = GlobalState(UInt64, key="cause_balance", description="Balance for the current cause")
+        self.cause_owner = GlobalState(Account, key="cause_owner", description="Owner of the cause")
 
     # -------------------------------
     # User Methods
@@ -29,12 +30,14 @@ class DonationContract(ARC4Contract):
     @arc4.abimethod(allow_actions=["OptIn"])
     def opt_in(self) -> None:
         """Opt into the contract to enable donation tracking for sender"""
-        self.donor_donations.set(Txn.sender, UInt64(0))
+        self.donor_donations[Txn.sender] = UInt64(0)
 
     @arc4.abimethod
     def register_cause(self, cause_name: arc4.String) -> arc4.UInt64:
         """Register a new cause with a unique ID"""
-        self.cause_count.set(self.cause_count.get() + UInt64(1))
+        count = self.cause_count.get()
+        self.cause_count.set(count + UInt64(1))
+
         cause_id = self.cause_count.get()
         self.cause_owner.set(Txn.sender)
         self.cause_balance.set(UInt64(0))
@@ -46,9 +49,15 @@ class DonationContract(ARC4Contract):
         assert amount.native > 0, "Donation amount must be greater than zero"
         assert cause_id.native <= self.cause_count.get(), "Invalid cause ID"
 
-        self.total_donations.set(self.total_donations.get() + amount.native)
-        self.donor_donations.set(Txn.sender, self.donor_donations.get(Txn.sender) + amount.native)
-        self.cause_balance.set(self.cause_balance.get() + amount.native)
+        total = self.total_donations.get()
+        self.total_donations.set(total + amount.native)
+
+        prev_donated = self.donor_donations[Txn.sender]
+        self.donor_donations[Txn.sender] = prev_donated + amount.native
+
+        balance = self.cause_balance.get()
+        self.cause_balance.set(balance + amount.native)
+
         return arc4.UInt64(self.total_donations.get())
 
     @arc4.abimethod
@@ -72,7 +81,7 @@ class DonationContract(ARC4Contract):
 
     @arc4.abimethod(readonly=True)
     def get_donor_donations(self) -> arc4.UInt64:
-        return arc4.UInt64(self.donor_donations.get(Txn.sender))
+        return arc4.UInt64(self.donor_donations[Txn.sender])
 
     @arc4.abimethod(readonly=True)
     def get_cause_balance(self) -> arc4.UInt64:
